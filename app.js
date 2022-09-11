db2ad44e785790dab3bf4754cbfad4b5a9e919b6
Muath <Muath.khalifa@yahoo.com>
2022-09-11 01:57:27 +0100

    finish css

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+
 const revenue = document.getElementById("revenue-input")
 const growth = document.getElementById("growth-input")
 const renewal = document.getElementById("renewal-input")
 const value = document.getElementById("value")
-
+const results = document.getElementById("results")
 
 
 value.addEventListener("click", ()=>{
-    console.log(revenue.value * (growth.value / 100) * 10 * (renewal.value / 100))
+    var value = revenue.value * (growth.value / 100) * 10 * (renewal.value / 100)
+
+    value =  Math.round((value + Number.EPSILON) * 100) /100;
+    value = value.toLocaleString("en-US")
+    results.innerText = value
 })
 
 // ARR x growth x 10 x renewal rate 
